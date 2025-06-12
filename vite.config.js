cb693aaf99cc90eec216c36bb6dcd9a29a6db8ad
Naresh.R <nareshrajaparimala000@gmail.com>
2025-06-12 23:26:01 +0530

    Deploy update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/QuestionPaperFormator-React-Latex-/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',   // 👈 Required for Docker
